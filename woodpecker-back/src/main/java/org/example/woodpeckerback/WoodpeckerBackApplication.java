f87b9ec0f40d970112a1f3b2f7c7557be9935ddd
Hajin <qwertygoov@naver.com>
2024-09-17 09:41:29 +0900

    init: 프로젝트 생성

---
+++ woodpecker-back/src/main/java/org/example/woodpeckerback/WoodpeckerBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.woodpeckerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodpeckerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoodpeckerBackApplication.class, args);
+    }
+
+}
---
+++ woodpecker-back/src/test/java/org/example/woodpeckerback/WoodpeckerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.woodpeckerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodpeckerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
