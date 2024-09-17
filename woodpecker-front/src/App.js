25867d6caa2533adbab9aa38d2e67dabf9ae96a2
Hajin <qwertygoov@naver.com>
2024-09-17 09:44:02 +0900

    del: .git 지우기

---
+++ woodpecker-front/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { useState } from 'react';
+
+const onKakaoLogin = () => {
+  window.location.href = "http://localhost:8080/oauth2/authorization/kakao"
+}
+
+const App = () => {
+  const [data, setData] = useState('');
+
+  const getData = () => {
+    fetch("http://localhost:8080/my", {
+      method: "GET",
+      credentials: 'include'
+    })
+    .then((res) => res.text()) 
+    .then((data) => {
+      console.log('Server Response:', data);  
+      document.getElementById('data').innerText = data;  
+    })
+    .catch((error) => {
+      console.error(error);
+      setData("Error occurred");
+    });
+  }
+
+  return (
+    <>
+      <button onClick={onKakaoLogin}>카카오 로그인</button>
+      <button onClick={getData}>데이터 가져오기</button>
+      <div id="data"></div>
+    </>
+  );
+}
+
+export default App;
