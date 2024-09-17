e1d0051611203e1e847a01f4304a6f17155b6d20
Hajin <qwertygoov@naver.com>
2024-09-17 10:28:29 +0900

    feat: Jwt 생성, 검증, 사용자 정보 추출하는 JwtService 정의

---
+++ woodpecker-back/src/main/java/org/example/woodpeckerback/service/JwtService.java
@@ -0,0 +1,43 @@
+package org.example.woodpeckerback.service;
+
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtService {
+
+    private SecretKey secretKey;
+
+    public JwtService(@Value("${SECRET}")String secret) {
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+
+    }
+
+    public String getUsername(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJwt(Long id, Long kakaoId, String username, Long expiredMs) {
+        return Jwts.builder()
+                .claim("id", id)
+                .claim("kakaoId", kakaoId)
+                .claim("username", username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
