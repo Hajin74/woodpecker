d2652d4db4be40275c51ff8a1208e9c6bc8fc866
Hajin <qwertygoov@naver.com>
2024-09-19 21:18:30 +0900

    feat: 저장한 책만 노트쓰게 하기

--- woodpecker-back/src/main/java/org/example/woodpeckerback/exception/ErrorCode.java
+++ woodpecker-back/src/main/java/org/example/woodpeckerback/exception/ErrorCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum ErrorCode {
     USER_NOT_FOUND("해당 사용자는 존재하지 않습니다."),
     BOOK_NOT_FOUND("해당 책은 존재하지 않습니다."),
+    BOOK_NOT_SAVED("저장한 책이 아닙니다."),
     BOOK_ALREADY_LIKED("이미 좋아요한 책입니다.");
 
     private final String errorMessage;
--- woodpecker-back/src/main/java/org/example/woodpeckerback/service/NoteService.java
+++ woodpecker-back/src/main/java/org/example/woodpeckerback/service/NoteService.java
@@ -9,6 +9,7 @@ import org.example.woodpeckerback.exception.CustomException;
 import org.example.woodpeckerback.exception.ErrorCode;
 import org.example.woodpeckerback.repository.BookRepository;
 import org.example.woodpeckerback.repository.NoteRepository;
+import org.example.woodpeckerback.repository.SaveBookRepository;
 import org.example.woodpeckerback.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +24,7 @@ public class NoteService {
     private final NoteRepository noteRepository;
     private final BookRepository bookRepository;
     private final UserRepository userRepository;
+    private final SaveBookRepository saveBookRepository;
 
     @Transactional
     public void saveNote(Long userId, String isbn, String content) {
@@ -30,6 +32,11 @@ public class NoteService {
                 () -> new CustomException(ErrorCode.USER_NOT_FOUND));
         Book book = bookRepository.findByIsbn(isbn).orElseThrow(
                 () -> new CustomException(ErrorCode.BOOK_NOT_FOUND));
+        boolean isSavedBook = saveBookRepository.existsByUserIdAndBookIsbn(userId, isbn);
+
+        if (!isSavedBook) {
+            throw new CustomException(ErrorCode.BOOK_NOT_SAVED);
+        }
 
         Note newNote = Note.builder()
                 .book(book)
